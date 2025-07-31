5ffae42a5db56df17742940ede91001027826f42
rishisakariya <rishi.sakariya@gmail.com>
2025-07-31 18:37:20 +0530

    promise

--- script.js
+++ script.js
@@ -1,55 +1,3 @@
-const solvePromiseObj = new Promise((res, rej) => {
-      const data = {
-            "id": 1,
-            "name": "Leanne Graham",
-            "username": "Bret",
-            "email": "Sincere@april.biz",
-            "address": {
-                  "street": "Kulas Light",
-                  "suite": "Apt. 556",
-                  "city": "Gwenborough",
-                  "zipcode": "92998-3874",
-                  "geo": {
-                        "lat": "-37.3159",
-                        "lng": "81.1496"
-                  }
-            },
-            "phone": "1-770-736-8031 x56442",
-            "website": "hildegard.org",
-            "company": {
-                  "name": "Romaguera-Crona",
-                  "catchPhrase": "Multi-layered client-server neural-net",
-                  "bs": "harness real-time e-markets"
-            }
-      }
-
-      res(JSON.stringify(data))
-});
-
-const rejectPromiseObj = new Promise((res, rej) => {
-      const data = {
-            message: "URL Invalid..."
-      }
-
-      rej(JSON.stringify(data)); // Rejected with JSON string
-});
-
-async function getDataFromPromise() {
-      try {
-            const response = await rejectPromiseObj
-            const data = JSON.parse(response)
-            console.log(data);
-
-      } catch (error) {
-            const parsedError = JSON.parse(error);
-            console.log(parsedError); // ➜ "URL Invalid...
-      }
-}
-
-getDataFromPromise()
-
-
-
 async function getData(url) {
       try {
             const response = await fetch(url)
@@ -61,6 +9,5 @@ async function getData(url) {
       }
 }
 
-// getData("https://jsonplaceholder.typicode.com/todos")
-// getData("https://jsonplaceholder.typicode.com/users")
-
+getData("https://jsonplaceholder.typicode.com/todos")
+getData("https://jsonplaceholder.typicode.com/users")
